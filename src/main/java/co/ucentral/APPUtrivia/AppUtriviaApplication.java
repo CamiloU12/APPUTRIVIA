f15a23545173d2b4b89517a3db57a5a7c0714c70
CAMILO <juribef@ucentral.edu.co>
2024-10-02 17:33:51 -0300

    (nuevo): estructura inicial

---
+++ src/main/java/co/ucentral/APPUtrivia/AppUtriviaApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.APPUtrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUtriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUtriviaApplication.class, args);
+	}
+
+}
